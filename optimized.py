ed805b2687d43662a7a3597faaec864571f47f46
noahwenck <123976574+noahwenck@users.noreply.github.com>
2024-04-03 11:29:06 -0500

    improved optimized code, result shows full disocclusion filled

--- optimized.py
+++ optimized.py
@@ -3,22 +3,26 @@ import numpy as np
 from binaryPixelSearch import binaryPixelSearch
 
 # IMAGE INPUT
-pixels = np.array(Image.open("images/optimizedBase.png"))
+pixels = np.array(Image.open("images/optimized/optimizedBase.png"))
 height, width, channels = pixels.shape
 
-# Hard-coded for optimizedBase.png example
-x = 136
-y = 325
-rangeToBeChecked = 8 # Keep even
+newImage = pixels.copy()
+rangeToBeChecked = 8  # Keep even
 
-for sourceX in range(x, width, rangeToBeChecked):
-    # Found possible edge of hole
-    if pixels[y][sourceX][3] > 0:
-        sourceX = binaryPixelSearch(pixels, y, sourceX-rangeToBeChecked, sourceX)
-        break
+for x in range(width):
+    for y in range(height):
 
-print("Edge Pixel found at: (" + str(sourceX) + ", " + str(y) + ")")
-pixels[y][x] = pixels[y][sourceX]
+        # Just doing right side no depth as an example.
+        # Only right side to show the distance it can cover like this.
+        # No depth since it doesn't matter with just one side, easy to implement depth.
+        if pixels[y][x][3] == 0:
+            for sourceX in range(x, width, rangeToBeChecked):
+                # Found possible edge of hole
+                if pixels[y][sourceX][3] > 0:
+                    sourceX = binaryPixelSearch(pixels, y, sourceX - rangeToBeChecked, sourceX)
+                    newImage[y][x] = pixels[y][sourceX]
+                    print("Edge Pixel found at: (" + str(sourceX) + ", " + str(y) + ")")
+                    break
 
 # IMAGE OUTPUT
-Image.fromarray(pixels, 'RGBA').save("images/optimizedResult.png", 'PNG')
+Image.fromarray(newImage, 'RGBA').save("images/optimized/optimizedResult.png", 'PNG')
